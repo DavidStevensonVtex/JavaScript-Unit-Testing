4b9c42d934b8efb6283492b0d472124f0a54253a
David Stevenson <drsgis@rit.edu>
2024-07-01 10:38:10 -0400

    package.json: Add --coverage option to jest. isPalindrome.js: Add hello function to test coverage.

--- Introduction-to-Jest-Testing/isPalindrome.js
+++ Introduction-to-Jest-Testing/isPalindrome.js
@@ -1,5 +1,9 @@
 const reverseString = require('./reverseString');
 
+function hello() {
+    return "Hello" ;
+}
+
 function isPalindrome(word) {
     return word.toLowerCase() == reverseString(word).toLowerCase();
 }
