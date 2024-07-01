4b641a38562e30e4b8ea6f5dd2d631c759eff618
David Stevenson <drsgis@rit.edu>
2024-07-01 08:37:55 -0400

    Install Dependencies for TypeScript - Further Instructions

--- JestJS/001_GettingStarted/Install-Dependencies-For-TypeScript.ps1
+++ JestJS/001_GettingStarted/Install-Dependencies-For-TypeScript.ps1
@@ -6,3 +6,18 @@
 # on using Babel above. Next, install the @babel/preset-typescript:
 
 npm install --save-dev @babel/preset-typescript
+
+# Then add @babel/preset-typescript to the list of presets in your babel.config.js.
+
+# babel.config.js
+# module.exports = {
+#   presets: [
+#     ['@babel/preset-env', {targets: {node: 'current'}}],
+#     '@babel/preset-typescript',
+#   ],
+# };
+
+# However, there are some caveats to using TypeScript with Babel. Because TypeScript support 
+# in Babel is purely transpilation, Jest will not type-check your tests as they are run. If 
+# you want that, you can use ts-jest instead, or just run the TypeScript compiler tsc 
+# separately (or as part of your build process).
