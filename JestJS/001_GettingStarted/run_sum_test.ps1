3677c6ecca45216c39c0273a033fa4cb20eda1a6
David Stevenson <drsgis@rit.edu>
2024-07-01 08:23:27 -0400

    Create sum function (sum.js). Create sum test (sum.test.js). Add test script to package.json. Run test using npm test.

---
+++ JestJS/001_GettingStarted/run_sum_test.ps1
@@ -0,0 +1,13 @@
+npm test
+
+# > test
+# > jest
+
+#  PASS  ./sum.test.js
+#   √ adds 1 + 2 to equal 3 (2 ms)
+
+# Test Suites: 1 passed, 1 total
+# Tests:       1 passed, 1 total
+# Snapshots:   0 total
+# Time:        0.412 s, estimated 1 s
+# Ran all test suites.
