34f2f31231ae8c72af6b359fcdffa4bbf9f4da36
David Stevenson <drsgis@rit.edu>
2024-07-01 08:34:42 -0400

    Install Dependencies for TypeScript

---
+++ JestJS/001_GettingStarted/Install-Dependencies-For-TypeScript.ps1
@@ -0,0 +1,8 @@
+# Using TypeScript
+
+# Via babel
+
+# Jest supports TypeScript, via Babel. First, make sure you followed the instructions 
+# on using Babel above. Next, install the @babel/preset-typescript:
+
+npm install --save-dev @babel/preset-typescript
