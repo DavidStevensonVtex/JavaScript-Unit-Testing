e527abe48facb15fc9278e93171569d0ae937b0c
David Stevenson <drsgis@rit.edu>
2024-07-01 08:29:32 -0400

    Additional Configuration
    Generate a basic configuration file
    
    Based on your project, Jest will ask you a few questions and will create a basic configuration file with a short description for each option

---
+++ JestJS/001_GettingStarted/Generate-A-Basic-Configuration-File.ps1
@@ -0,0 +1,20 @@
+# Additional Configuration
+
+# Generate a basic configuration file
+
+# Based on your project, Jest will ask you a few questions and will create 
+# a basic configuration file with a short description for each option:
+
+npm init jest@latest
+
+# .\Generate-A-Basic-Configuration-File.ps1
+
+# The following questions will help Jest to create a suitable configuration for your project
+
+# √ Would you like to use Typescript for the configuration file? ... no
+# √ Choose the test environment that will be used for testing » node
+# √ Do you want Jest to add coverage reports? ... yes
+# √ Which provider should be used to instrument code for coverage? » v8
+# √ Automatically clear mock calls, instances, contexts and results before every test? ... yes
+
+# 📝  Configuration file created at C:\Users\drsgis\drs\JavaScript-Unit-Testing\JestJS\001_GettingStarted\jest.config.js
