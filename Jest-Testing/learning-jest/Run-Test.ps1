84ef19058ac6c9ed72b62f402921cc5a9eac1695
David Stevenson <drsgis@rit.edu>
2024-07-01 09:34:46 -0400

    Run Your First Test
    You’re now ready to run your first test. Back to your terminal, simply run npm test.

---
+++ Jest-Testing/learning-jest/Run-Test.ps1
@@ -0,0 +1,17 @@
+npm test
+
+# > learning-jest@1.0.0 test
+# > jest
+
+#  PASS  ./index.test.js
+#   FizzBuzz
+#     √ [3] should result in "fizz" (2 ms)
+#     √ [5] should result in "buzz"
+#     √ [15] should result in "fizzbuzz" (1 ms)
+#     √ [1,2,3] should result in "1, 2, fizz"
+
+# Test Suites: 1 passed, 1 total
+# Tests:       4 passed, 4 total
+# Snapshots:   0 total
+# Time:        0.543 s
+# Ran all test suites.
