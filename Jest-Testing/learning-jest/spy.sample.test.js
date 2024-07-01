15ca46fd83172f4f32d862e6d024bc0f585df477
David Stevenson <drsgis@rit.edu>
2024-07-01 09:44:19 -0400

    Spy Sample

---
+++ Jest-Testing/learning-jest/spy.sample.test.js
@@ -0,0 +1,11 @@
+const video = require('./video');
+
+test('plays video', () => {
+const spy = jest.spyOn(video, 'play');
+const isPlaying = video.play();
+
+expect(spy).toHaveBeenCalled();
+expect(isPlaying).toBe(true);
+
+spy.mockRestore();
+});
