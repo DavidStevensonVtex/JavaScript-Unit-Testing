10eca7a33c5d7b8af1abda832631f9e6930808a4
David Stevenson <drsgis@rit.edu>
2024-07-01 08:40:56 -0400

    Installing ts-jest dependencies

---
+++ JestJS/001_GettingStarted/Using-ts-jest.ps1
@@ -0,0 +1,9 @@
+# Via ts-jest
+
+# ts-jest is a TypeScript preprocessor with source map support for Jest that 
+# lets you use Jest to test projects written in TypeScript.
+
+npm install --save-dev ts-jest
+
+# In order for Jest to transpile TypeScript with ts-jest, you may also need
+# to create a configuration file.
