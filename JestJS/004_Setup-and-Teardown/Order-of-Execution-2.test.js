4a1678a0ba57d06fc554b52b335896800fcd70e8
David Stevenson <drsgis@rit.edu>
2024-07-02 10:54:52 -0400

    Order of Execution - 2
    Just like the describe and test blocks Jest calls the before* and after* hooks in the order of declaration. Note that the after* hooks of the enclosing scope are called first. For example, here is how you can set up and tear down resources which depend on each other:

---
+++ JestJS/004_Setup-and-Teardown/Order-of-Execution-2.test.js
@@ -0,0 +1,35 @@
+// Order of Execution
+
+// Just like the describe and test blocks Jest calls the before* and after* hooks 
+// in the order of declaration. Note that the after* hooks of the enclosing scope 
+// are called first. For example, here is how you can set up and tear down resources 
+// which depend on each other:
+
+beforeEach(() => console.log('connection setup'));
+beforeEach(() => console.log('database setup'));
+
+afterEach(() => console.log('database teardown'));
+afterEach(() => console.log('connection teardown'));
+
+test('test 1', () => console.log('test 1'));
+
+describe('extra', () => {
+  beforeEach(() => console.log('extra database setup'));
+  afterEach(() => console.log('extra database teardown'));
+
+  test('test 2', () => console.log('test 2'));
+});
+
+// connection setup
+// database setup
+// test 1
+// database teardown
+// connection teardown
+
+// connection setup
+// database setup
+// extra database setup
+// test 2
+// extra database teardown
+// database teardown
+// connection teardown
