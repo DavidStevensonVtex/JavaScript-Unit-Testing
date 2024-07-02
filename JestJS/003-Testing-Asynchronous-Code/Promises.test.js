31701b62d1ac26e8ed26c2c92a8b0ddc0bfdfd68
David Stevenson <drsgis@rit.edu>
2024-07-02 08:25:31 -0400

    Testing Asynchronous Code
    It's common in JavaScript for code to run asynchronously. When you have code that runs asynchronously, Jest needs to know when the code it is testing has completed, before it can move on to another test. Jest has several ways to handle this.
    
    Promises
    
    Return a promise from your test, and Jest will wait for that promise to resolve. If the promise is rejected, the test will fail.

---
+++ JestJS/003-Testing-Asynchronous-Code/Promises.test.js
@@ -0,0 +1,43 @@
+// Testing Asynchronous Code
+
+// It's common in JavaScript for code to run asynchronously. When you have code that runs asynchronously,
+// Jest needs to know when the code it is testing has completed, before it can move on to another test. 
+// Jest has several ways to handle this.
+
+// Promises
+
+// Return a promise from your test, and Jest will wait for that promise to resolve. If the promise is 
+// rejected, the test will fail.
+
+// For example, let's say that fetchData returns a promise that is supposed to resolve to the string 'peanut butter'. 
+// We could test it with:
+
+function fetchData() {
+    const myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // console.log("issuing resolve");
+            resolve("peanut butter");
+        }, 300);
+    });
+
+    return myPromise.then(function (data) {
+        // console.log("myPromise", "data", data);
+        return data;
+    },
+    function () {
+        throw new Error("An error happened");
+    });
+}
+
+// fetchData().then(data => {
+//     console.log("data", data);
+//     // expect(data).toBe('peanut butter');
+// });
+
+
+test('the data is peanut butter', () => {
+    return fetchData().then(data => {
+        // console.log("data", data);
+        expect(data).toBe('peanut butter');
+    });
+});
