9a7cf63f737b7da1dee9d156d091779ead26497e
David Stevenson <drsgis@rit.edu>
2024-07-09 13:26:28 -0400

    jest.disableAutomock - update test results and documentation

--- JestJS/Jest-API/The-Jest-Object/__tests__/disableAutomocking.test.js
+++ JestJS/Jest-API/The-Jest-Object/__tests__/disableAutomocking.test.js
@@ -1,3 +1,7 @@
+// jest.disableAutomock()
+
+// Disables automatic mocking in the module loader.
+
 const utils = require('../utils');
 
 jest.disableAutomock();
@@ -7,3 +11,9 @@ test('original implementation', () => {
   // even if we set the automocking in a jest configuration
   expect(utils.authorize()).toBe('token');
 });
+
+// PASS  __tests__/disableAutomocking.test.js
+// √ original implementation (2 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
